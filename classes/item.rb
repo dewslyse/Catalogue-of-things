fe442cdd2ff9516580d3e154c57ab7e57d2bf254
dewslyse <dewslyse@gmail.com>
2022-06-27 20:58:28 +0000

    Add custom setter methods

--- classes/item.rb
+++ classes/item.rb
@@ -1,6 +1,9 @@
 require 'date'
 
 class Item
+  attr_reader :id, archived
+  attr_accessor :author, :genre, :label
+
   def initialize(id, published_date, archived: false)
     @id = id
     @published_date = Date.parse(published_date)
@@ -13,5 +16,20 @@ class Item
 
   def move_to_archive
     @archived = can_be_archived?
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
   end
 end
