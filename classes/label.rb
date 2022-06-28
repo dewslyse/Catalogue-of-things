e60785dde2a2749e1a5a05955215ce6444fbc7d2
tafaramafemba <tafara.maf@gmail.com>
2022-06-28 18:58:47 +0200

    Fix linter errors

--- classes/label.rb
+++ classes/label.rb
@@ -1,5 +1,5 @@
 class Label
-  attr_accessor :title, :color :items
+  attr_accessor :title, :color, :items
   attr_reader :id
 
   def initialize(id, title, color)
--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -2,11 +2,10 @@ require_relative '../classes/musicalbum'
 
 describe MusicAlbum do
   context 'It should check if music album is on spotify' do
-    album = MusicAlbum.new()
+    album = MusicAlbum.new
 
     it 'should create an instance of class MusicAlbum' do
       expect(album).to be_an_instance_of(MusicAlbum)
     end
-
   end
 end
