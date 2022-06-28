f1c1ce3680eebba7496c402f8cee3217db25229a
dewslyse <dewslyse@gmail.com>
2022-06-28 21:18:35 +0000

    Add tests for Book class

--- classes/book.rb
+++ classes/book.rb
@@ -2,6 +2,7 @@ require_relative 'item'
 
 class Book < Item
   attr_reader :publisher, :cover_state
+  attr_accessor :published_date
 
   def initialize(id, published_date, publisher, cover_state)
     super(id, published_date)
@@ -13,3 +14,11 @@ class Book < Item
     super || @cover_state == 'bad'
   end
 end
+
+book1 = Book.new(12, '2018-03-14', 'Cambridge', 'bad')
+puts book1
+puts book1.id
+puts book1.published_date
+puts book1.publisher
+puts book1.cover_state
+print book1.can_be_archived?
---
+++ spec/book_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../classes/book'
+
+describe Book do
+  context 'Testing Book class' do
+    before :each do
+      @book1 = Book.new(12, '2010-03-14', 'Cambridge', 'good')
+      @book2 = Book.new(16, '2020-01-24', 'Oxford', 'good')
+      @book3 = Book.new(18, '2018-10-06', 'Penguin', 'bad')
+    end
+
+    it 'returns instance of Book' do
+      expect(@book1).to be_instance_of(Book)
+    end
+
+    it 'returns published date' do
+      expect(@book1.published_date).to eq Date.parse('2010-03-14')
+    end
+
+    it 'checks if book can be achived' do
+      expect(@book1.can_be_archived?).to eq true
+    end
+
+    it 'checks if book can be achived' do
+      expect(@book2.can_be_archived?).to eq false
+    end
+
+    it 'checks if book can be achived' do
+      expect(@book3.can_be_archived?).to eq true
+    end
+  end
+end
