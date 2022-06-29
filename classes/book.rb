3f355d7cb47afe7ccb45d8ab8f96f568ed473ee0
dewslyse <dewslyse@gmail.com>
2022-06-29 02:46:28 +0000

    Remove test code

--- classes/book.rb
+++ classes/book.rb
@@ -15,11 +15,3 @@ class Book < Item
     super || @cover_state == 'bad'
   end
 end
-
-book1 = Book.new('2018-03-14', 'Cambridge', 'bad')
-puts book1
-puts book1.id
-puts book1.published_date
-puts book1.publisher
-puts book1.cover_state
-print book1.can_be_archived?
