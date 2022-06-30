d2f09330a82eee5dc0ed3af4fb735e1d3afbc5b0
dewslyse <dewslyse@gmail.com>
2022-06-30 15:56:23 +0000

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,24 +1,51 @@
-CREATE TABLE "Items"
+CREATE TABLE items
 (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    genre "char",
-    author "char",
-    source "char",
-    label "char",
     publish_date date,
-    archived boolean
+    archived boolean,
+    genre_id "char",
+    author_id "char",
+    label_id "char",
+    FOREIGN KEY (genre_id) REFERENCES genre (id),
+    FOREIGN KEY (author_id) REFERENCES author (id),
+    FOREIGN KEY (label_id) REFERENCES label (id)
 );
 
-CREATE TABLE "Genre"
+CREATE TABLE music_album
 (
-    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name "char",
+    id integer,
+    on_spotify boolean,
+    FOREIGN KEY (id) REFERENCES items (id)
 );
 
-CREATE TABLE "MusicAlbum"
+CREATE TABLE genre
 (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    genre "char",
-    publish_date date,
-    on_spotify boolean
+    name "char"
+);
+
+CREATE TABLE book (
+    id INT,
+    publihser TEXT,
+    cover_state TEXT,
+    FOREIGN KEY (id) REFERENCES items (id)
+);
+
+CREATE TABLE label (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    title TEXT,
+    color TEXT
+);
+
+CREATE TABLE game (
+    id INT,
+    mutiplayer TEXT,
+    last_played_at DATE,
+    FOREIGN KEY (id) REFERENCES items (id)
+);
+
+CREATE TABLE author (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    first_name TEXT,
+    last_name TEXT
 );
